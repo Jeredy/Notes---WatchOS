afe6d8ad1cd1ec5a52a9f733980283b8c20fcda3
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-27 13:22:47 -0300

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by André Almeida on 2022-07-27.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
